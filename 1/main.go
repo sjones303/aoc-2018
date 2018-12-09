e995f97835db8c1e734b6112a385485720dcd542
Simon Jones <eighteenoseven@gmail.com>
2018-12-09 21:31:19 +0000

    01-1 Implement argument parsing

--- 1/main.go
+++ 1/main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
-	"errors"
+	"fmt"
+	"strconv"
 )
 
 func parse(in []string) ([]int, error) {
-	return nil, errors.New("not implemented")
+	out := make([]int, len(in))
+	for i, v := range in {
+		var err error
+		out[i], err = strconv.Atoi(v)
+		if err != nil {
+			return nil, fmt.Errorf("arg %d: %v", i, err)
+		}
+	}
+	return out, nil
 }
