58c29ec272426364a1a88aca0fb72971bf72c894
Simon Jones <eighteenoseven@gmail.com>
2018-12-09 21:42:32 +0000

    01-1 Implement final solution

--- 1/main.go
+++ 1/main.go
@@ -1,9 +1,23 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
 	"strconv"
+
+	"github.com/sjones303/aoc-2018/1/freq"
 )
+
+func main() {
+	flag.Parse()
+	in, err := parse(flag.Args())
+	if err != nil {
+		fmt.Fprint(os.Stderr, err)
+		os.Exit(1)
+	}
+	fmt.Printf("sum: %d\n", freq.Sum(in))
+}
 
 func parse(in []string) ([]int, error) {
 	out := make([]int, len(in))
