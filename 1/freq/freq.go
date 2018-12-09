16a212bfd03364d229c8b089275a23b05eda031e
Simon Jones <eighteenoseven@gmail.com>
2018-12-09 21:07:19 +0000

    01-1 Implement Sum

--- 1/freq/freq.go
+++ 1/freq/freq.go
@@ -1,5 +1,13 @@
 package freq
 
 func Sum(in []int) int {
-	return 0
+	return sum(in[0], in[1:])
+}
+
+func sum(f int, r []int) int {
+	v := f + r[0]
+	if len(r) == 1 {
+		return v
+	}
+	return sum(v, r[1:])
 }
