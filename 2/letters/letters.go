9e2c49ca6d448bcead6d37c26fc33ac062e40a97
Simon Jones <eighteenoseven@gmail.com>
2018-12-10 22:10:04 +0000

    02-1 Implement Count

--- 2/letters/letters.go
+++ 2/letters/letters.go
@@ -1,5 +1,20 @@
 package letters
 
 func Count(in string) (c2, c3 bool) {
-	return false, false
+	l := make(map[rune]int, 26)
+	for _, r := range in {
+		l[r]++
+	}
+	for _, c := range l {
+		if c == 2 {
+			c2 = true
+		}
+		if c == 3 {
+			c3 = true
+		}
+		if c2 && c3 {
+			return c2, c3
+		}
+	}
+	return c2, c3
 }
