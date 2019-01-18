e0e0e1022e3f1e15ecd349d8a4810a66164210e1
Simon Jones <eighteenoseven@gmail.com>
2019-01-18 20:27:23 +0000

    WIP: Implement event.Parse

--- 4/event/event.go
+++ 4/event/event.go
@@ -2,6 +2,8 @@ package event
 
 import (
 	"errors"
+	"fmt"
+	"strings"
 	"time"
 )
 
@@ -20,5 +22,36 @@ type Event struct {
 }
 
 func Parse(in string) (Event, error) {
-	return Event{}, errors.New("not implemented")
+	e := Event{}
+
+	n := strings.LastIndex(in, "]")
+	if n < 0 {
+		return e, errors.New("unable to find end of time")
+	}
+
+	var err error
+	e.T, err = time.Parse("[2006-01-02 15:04]", in[:n+1])
+	if err != nil {
+		return e, fmt.Errorf("malformed time: %v", err)
+	}
+
+	switch {
+	case strings.Contains(in[n+2:], "falls asleep"):
+		e.A = FallAsleep
+	case strings.Contains(in[n+2:], "wakes up"):
+		e.A = WakeUp
+	default:
+		p := 0
+		p, err := fmt.Sscanf(in[n+2:], "Guard #%d begins shift", &e.G)
+		switch {
+		case err != nil:
+			return e, fmt.Errorf("bad start action: %v", err)
+		case p != 1:
+			return e, fmt.Errorf(
+				"bad start action: %d/1 items found", p)
+		}
+		e.A = Start
+	}
+
+	return e, nil
 }
